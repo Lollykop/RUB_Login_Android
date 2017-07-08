11aa6d948762cb137ddaf78691621776b378b733
Christian Ocepek <christianocepek@yahoo.de>
2017-07-08 19:56:13 +0200

    first commit

---
+++ src/main/java/com/example/lollykop/rublogin/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.lollykop.rublogin;
+
+import android.content.SharedPreferences;
+import android.support.v4.content.SharedPreferencesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.duration;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button btn;
+    TextView username;
+    TextView password;
+    SharedPreferences mySPR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn_save);
+        username = (TextView) findViewById(R.id.t_username);
+        password = (TextView) findViewById(R.id.t_password);
+
+
+        mySPR = getSharedPreferences("Data",0);
+
+        username.setText(mySPR.getString("username","UserName"));
+        password.setText(mySPR.getString("password","Password"));
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                SharedPreferences.Editor editor = mySPR.edit();
+                editor.putString("username",username.getText().toString());
+                editor.putString("password",password.getText().toString());
+                editor.commit();
+
+                Toast toast = Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+    }
+}
---
+++ src/main/java/com/example/lollykop/rublogin/WidgetProverder_Login.java
@@ -0,0 +1,109 @@
+package com.example.lollykop.rublogin;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Lollykop on 7/6/2017.
+ */
+
+public class WidgetProverder_Login extends AppWidgetProvider
+{
+    Button btn;
+    final String LOGIN ="asklfdaskfjas";
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.widget_layout);
+
+        remoteViews.setOnClickPendingIntent(R.id.btn_widget_Login,onClickPendingInten(context,LOGIN));
+        appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        if(intent.getAction().toString()==LOGIN)
+        {
+            Login(context);
+        }
+
+        super.onReceive(context, intent);
+    }
+
+    public PendingIntent onClickPendingInten(Context context,String stringAction)
+    {
+        Intent onClickIntent = new Intent(context,WidgetProverder_Login.class);
+        onClickIntent.setAction(stringAction);
+        return PendingIntent.getBroadcast(context,0,onClickIntent,0);
+    }
+
+    public void Login(final Context context)
+    {
+        System.out.println("START TO LOGIN");
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+        //this is the url where you want to send the request
+        //TODO: replace with your own url to send request, as I am using my own localhost for this tutorial
+        String url = "https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response string.
+                        System.out.println(response);
+                        Toast toast = Toast.makeText(context, "Logged in!", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        }) {
+            //adding parameters to the request
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<>();
+
+                SharedPreferences mySPR = context.getSharedPreferences("Data",0);
+                String username =mySPR.getString("username","UserName");
+                String password =mySPR.getString("password","Password");
+
+                params.put("code", "1&");
+                params.put("loginid", username);
+                params.put("password",password);
+                params.put("ipaddr", "");
+                params.put("action", "Login");
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
+
