98ab7ba044d2752b9b1f230bde955ad6725b7344
Christian Ocepek <christianocepek@yahoo.de>
2017-07-24 19:36:24 +0200

    added autologin

--- src/main/java/com/example/lollykop/rublogin/MainActivity.java
+++ src/main/java/com/example/lollykop/rublogin/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity
     Button btn;
     TextView username;
     TextView password;
+    CheckBox autologin;
     SharedPreferences mySPR;
 
     @Override
@@ -28,12 +30,14 @@ public class MainActivity extends AppCompatActivity
         btn = (Button) findViewById(R.id.btn_save);
         username = (TextView) findViewById(R.id.t_username);
         password = (TextView) findViewById(R.id.t_password);
+        autologin= (CheckBox) findViewById(R.id.cbAutoLogin);
 
 
         mySPR = getSharedPreferences("Data",0);
 
         username.setText(mySPR.getString("username","UserName"));
         password.setText(mySPR.getString("password","Password"));
+        autologin.setChecked(mySPR.getBoolean("autologin",false));
 
         btn.setOnClickListener(new View.OnClickListener()
         {
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity
                 SharedPreferences.Editor editor = mySPR.edit();
                 editor.putString("username",username.getText().toString());
                 editor.putString("password",password.getText().toString());
+                editor.putBoolean("autologin",autologin.isChecked());
                 editor.commit();
 
                 Toast toast = Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_LONG);
--- src/main/java/com/example/lollykop/rublogin/WidgetProverder_Login.java
+++ src/main/java/com/example/lollykop/rublogin/WidgetProverder_Login.java
@@ -6,6 +6,13 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.RemoteViews;
 import android.widget.Toast;
@@ -21,6 +28,8 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by Lollykop on 7/6/2017.
  */
@@ -35,6 +44,16 @@ public class WidgetProverder_Login extends AppWidgetProvider
 
         remoteViews.setOnClickPendingIntent(R.id.btn_widget_Login,onClickPendingInten(context,LOGIN));
         appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
+
+
+
+        SharedPreferences mySPR = context.getSharedPreferences("Data",0);
+        boolean autologin =mySPR.getBoolean("autoLogin",false);
+
+        if(autologin)
+        Login(context);
+
+
 
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
@@ -57,61 +76,90 @@ public class WidgetProverder_Login extends AppWidgetProvider
         return PendingIntent.getBroadcast(context,0,onClickIntent,0);
     }
 
-    public void Login(final Context context)
-    {
-        System.out.println("START TO LOGIN");
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(context);
-        //this is the url where you want to send the request
-        String url = "https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin";
+    public static void forceConnectionToWifi(Context context) {
+        final ConnectivityManager connection_manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            NetworkRequest.Builder request = new NetworkRequest.Builder();
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the response string.
-                        System.out.println(response);
-                        Toast toast;
-                        if(response.contains("Authentisierung gelungen"))
-                            toast = Toast.makeText(context, "Logged in!", Toast.LENGTH_SHORT);
-                        else if(response.contains("loginID oder Passwort falsch"))
-                            toast = Toast.makeText(context, "loginID oder Passwort falsch!", Toast.LENGTH_LONG);
-                        else
-                            toast = Toast.makeText(context, "unknown Error?!", Toast.LENGTH_LONG);
+            Log.d(TAG,"request TRANSPORT_CELLULAR");
+            request.addCapability(NetworkCapabilities.TRANSPORT_WIFI);
 
-                        toast.show();
-
+            connection_manager.requestNetwork(request.build(), new ConnectivityManager.NetworkCallback() {
+                @Override
+                public void onAvailable(Network network) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        Log.d(TAG,"binding app to cellular network");
+                        connection_manager.bindProcessToNetwork(network);
                     }
-                }, new Response.ErrorListener() {
+                }
+            });
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast toast = Toast.makeText(context, "unknown Error?!", Toast.LENGTH_LONG);
-                toast.show();
-           }
-        }) {
-            //adding parameters to the request
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError
-            {
-                Map<String, String> params = new HashMap<>();
-
-                SharedPreferences mySPR = context.getSharedPreferences("Data",0);
-                String username =mySPR.getString("username","UserName");
-                String password =mySPR.getString("password","Password");
-
-                params.put("code", "1&");
-                params.put("loginid", username);
-                params.put("password",password);
-                params.put("ipaddr", "");
-                params.put("action", "Login");
-                return params;
-            }
-        };
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        }
     }
 
+    public void Login(final Context context)
+    {
+        WifiManager mainWifiObj;
+        mainWifiObj = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        if(mainWifiObj.isWifiEnabled()&&mainWifiObj.getConnectionInfo()!=null)
+        {
+
+            forceConnectionToWifi(context);
+
+            System.out.println("START TO LOGIN");
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(context);
+            //this is the url where you want to send the request
+            String url = "https://login.rz.ruhr-uni-bochum.de/cgi-bin/laklogin";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the response string.
+                            System.out.println(response);
+                            Toast toast;
+                            if(response.contains("Authentisierung gelungen"))
+                                toast = Toast.makeText(context, "Logged in!", Toast.LENGTH_SHORT);
+                            else if(response.contains("loginID oder Passwort falsch"))
+                                toast = Toast.makeText(context, "loginID oder Passwort falsch!", Toast.LENGTH_LONG);
+                            else
+                                toast = Toast.makeText(context, "unknown Error?!", Toast.LENGTH_LONG);
+
+                            toast.show();
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast toast = Toast.makeText(context, "unknown Error?!", Toast.LENGTH_LONG);
+                    toast.show();
+               }
+            }) {
+                //adding parameters to the request
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError
+                {
+                    Map<String, String> params = new HashMap<>();
+
+                    SharedPreferences mySPR = context.getSharedPreferences("Data",0);
+                    String username =mySPR.getString("username","UserName");
+                    String password =mySPR.getString("password","Password");
+
+                    params.put("code", "1&");
+                    params.put("loginid", username);
+                    params.put("password",password);
+                    params.put("ipaddr", "");
+                    params.put("action", "Login");
+                    return params;
+                }
+            };
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        }
+    }
 }
 
